e6909ba5f6741c7787f990d7fed8325735d68f53
Adarsh Dwivedi <adi3799adi@gmail.com>
2024-08-14 11:58:29 +0530

    Transmission changes has been done

--- Frontend/src/pages/private/Transmission/FeederBay/AddFeederBayData.jsx
+++ Frontend/src/pages/private/Transmission/FeederBay/AddFeederBayData.jsx
@@ -1,8 +1,57 @@
+import React, { useState } from "react";
 import FormPanel from "../../../../component/FormPanel";
 import Header from "../../../../component/Header";
-import { btn, input, label, select } from "../../../../utils/tailwindClasses";
+import {
+  btn,
+  input,
+  label,
+  removebtn,
+  select,
+} from "../../../../utils/tailwindClasses";
+import Table, { Tbody, Td, Th, Thead, Tr } from "../../../../component/Table";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/solid";
 
 function AddFeederBayData() {
+  const [rows, setRows] = useState([
+    {
+      id: Date.now(),
+      energyMeter: "",
+      mf: "",
+      openingReadingImport: "",
+      openingReadingExport: "",
+      month: "",
+      year: "",
+      isAbandon: "",
+    },
+  ]);
+
+  const handleAddRow = () => {
+    setRows([
+      ...rows,
+      {
+        id: Date.now(),
+        energyMeter: "",
+        mf: "",
+        openingReadingImport: "",
+        openingReadingExport: "",
+        month: "",
+        year: "",
+        isAbandon: "",
+      },
+    ]);
+  };
+
+  const handleRemoveRow = (id) => {
+    setRows(rows.filter((row) => row.id !== id));
+  };
+
+  const handleInputChange = (id, field, value) => {
+    const updatedRows = rows.map((row) =>
+      row.id === id ? { ...row, [field]: value } : row
+    );
+    setRows(updatedRows);
+  };
+
   return (
     <>
       <Header
@@ -15,7 +64,7 @@ function AddFeederBayData() {
       <FormPanel>
         <div className="col-span-1">
           <div className="relative z-0 w-full group">
-            <label className={label}>Zone</label>
+            <label className={label}>Zone (Transmission)</label>
             <select className={select} defaultValue="">
               <option>--Select--</option>
             </select>
@@ -23,7 +72,7 @@ function AddFeederBayData() {
         </div>
         <div className="col-span-1">
           <div className="relative z-0 w-full group">
-            <label className={label}>Circle</label>
+            <label className={label}>Circle (Transmission)</label>
             <select className={select} defaultValue="">
               <option>--Select--</option>
             </select>
@@ -31,7 +80,7 @@ function AddFeederBayData() {
         </div>
         <div className="col-span-1">
           <div className="relative z-0 w-full group">
-            <label className={label}>Division</label>
+            <label className={label}>Division (Transmission)</label>
             <select className={select} defaultValue="">
               <option>--Select--</option>
             </select>
@@ -39,24 +88,48 @@ function AddFeederBayData() {
         </div>
         <div className="col-span-1">
           <div className="relative z-0 w-full group">
-            <label className={label}>District</label>
-            <select className={select} defaultValue="">
-              <option>--Select--</option>
-            </select>
-          </div>
-        </div>
-        <div className="col-span-1">
-          <div className="relative z-0 w-full group">
-            <input className={input} placeholder=" " />
             <label className={label}>Name Of Substation</label>
+            <select className={select} defaultValue="">
+              <option>--Select--</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="relative z-0 w-full group">
+            <label className={label}>Voltage Level Of Feeder/Bay</label>
+            <select className={select} defaultValue="">
+              <option>--Select--</option>
+            </select>
           </div>
         </div>
         <div className="col-span-1">
           <div className="relative z-0 w-full group">
             <input className={input} placeholder=" " />
-            <label className={label}>
-              Existing Voltage Level Of Substation (K.V){" "}
-            </label>
+            <label className={label}>Name Of Feeder/Bay Of Substation</label>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="relative z-0 w-full group">
+            <label className={label}>Interface Type</label>
+            <select className={select} defaultValue="">
+              <option>--Select--</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="relative z-0 w-full group">
+            <label className={label}>Transaction Type</label>
+            <select className={select} defaultValue="">
+              <option>--Select--</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-span-1">
+          <div className="relative z-0 w-full group">
+            <label className={label}>Discom (Transmission)</label>
+            <select className={select} defaultValue="">
+              <option>--Select--</option>
+            </select>
           </div>
         </div>
 
@@ -64,7 +137,121 @@ function AddFeederBayData() {
           <button className={btn + " w-1/5"}>Submit</button>
         </div>
       </FormPanel>
+
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Energy Meter Serial No</Th>
+            <Th>MF(MWh)</Th>
+            <Th>Opening Reading(Import)</Th>
+            <Th>Opening Reading(Export)</Th>
+            <Th>Month</Th>
+            <Th>Year</Th>
+            <Th>Is Abandon</Th>
+            <Th>Action</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {rows.map((row, index) => (
+            <Tr key={row.id}>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.energyMeter}
+                  onChange={(e) =>
+                    handleInputChange(row.id, "energyMeter", e.target.value)
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.mf}
+                  onChange={(e) =>
+                    handleInputChange(row.id, "mf", e.target.value)
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.openingReadingImport}
+                  onChange={(e) =>
+                    handleInputChange(
+                      row.id,
+                      "openingReadingImport",
+                      e.target.value
+                    )
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.openingReadingExport}
+                  onChange={(e) =>
+                    handleInputChange(
+                      row.id,
+                      "openingReadingExport",
+                      e.target.value
+                    )
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.month}
+                  onChange={(e) =>
+                    handleInputChange(row.id, "month", e.target.value)
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.year}
+                  onChange={(e) =>
+                    handleInputChange(row.id, "year", e.target.value)
+                  }
+                />
+              </Td>
+              <Td>
+                <input
+                  className={input}
+                  placeholder=" "
+                  value={row.isAbandon}
+                  onChange={(e) =>
+                    handleInputChange(row.id, "isAbandon", e.target.value)
+                  }
+                />
+              </Td>
+              <Td flex={true}>
+                {index === 0 ? (
+                  <button className={btn} onClick={handleAddRow}>
+                    Add
+                  </button>
+                ) : (
+                  <button
+                    className={removebtn + " bg-red-500"}
+                    onClick={() => handleRemoveRow(row.id)}
+                  >
+                    Remove
+                  </button>
+                )}
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </>
   );
 }
+
 export default AddFeederBayData;
--- Frontend/src/pages/private/Transmission/FeederBay/FeederBayDataList.jsx
+++ Frontend/src/pages/private/Transmission/FeederBay/FeederBayDataList.jsx
@@ -21,7 +21,11 @@ function FeederBayDataList() {
             <Th>Circle</Th>
             <Th>Division</Th>
             <Th>Name of Substation</Th>
-            <Th>Existing Voltage Level Of Substation</Th>
+            <Th>Voltage Level of Feeder/Bay</Th>
+            <Th>Name Of Feeder/Bay Of Substation</Th>
+            <Th>Interface Type</Th>
+            <Th>Trasaction Type</Th>
+            <Th>Discom</Th>
             <Th>Action</Th>
           </Tr>
         </Thead>
@@ -33,8 +37,11 @@ function FeederBayDataList() {
             <Td>ETC Prayagraj-1</Td>
             <Td>ETD Prayagraj-1</Td>
             <Td>132 KV JHUNSI</Td>
-            <Td>132-133</Td>
-
+            <Td>33</Td>
+            <Td>63 MVA T/F APEX Main</Td>
+            <Td>T-T</Td>
+            <Td>Intra State</Td>
+            <Td>With In Zone</Td>
             <Td flex={true}>
               <PencilSquareIcon className="h-5 mt-3 w-5 ms-2" />
             </Td>
